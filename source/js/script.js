14c52d8ef4ad18e6c76e3c02d42a48a611859f21
drmonro <rapman477@ya.ru>
2019-12-23 05:03:57 +0700

    Товары и услуги

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,9 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+window.onload = function () {
+  document.body.innerHTML = document.body.innerHTML.replace(/\u2028/g, '');
+};
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+
+
+
